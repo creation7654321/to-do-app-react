3928b1e63c370cfb8623078f022d1f92d54965aa
creation7654321 <creation.pradhan@gmail.com>
2024-08-12 17:30:22 +0545

    resolving error

--- src/components/TodoWrapper.jsx
+++ src/components/TodoWrapper.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TodoForm from './TodoForm'
 import{v4 as uuidv4} from 'uuid'
 import Todo from './Todo';
@@ -6,10 +6,13 @@ import EditTodoForm from './EditTodoForm';
 uuidv4();
 export default function TodoWrapper() {
     const[todos, setTodos] = useState([])
+
+
    
     const addTodo = todo =>{
         setTodos([...todos, {id:uuidv4(), task:todo, completed:false, isEditing:false}])
-        console.log(todos)
+      
+        
     }
     const toggleComplete = id =>{
         setTodos(todos.map(todo => todo.id === id?
@@ -27,7 +30,8 @@ export default function TodoWrapper() {
             {...todo, task, isEditing:!todo.isEditing}:todo
         ))
     }
-  
+    
+
 
   return (
     <div className='TodoWrapper'>
